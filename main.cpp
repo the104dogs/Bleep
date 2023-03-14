8713101e5469d224c6f6ef0322f5bc45a3c9f47f
Roni <120666460+the104dogs@users.noreply.github.com>
2023-03-14 16:57:25 +0000

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+int main () {
+
+using namespace std;
+
+string word = "broccolli";
+string text = " I eat broccolli all the time it is the best broccolli i will ever eat, broccolli all the time it is the best broccolli";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
